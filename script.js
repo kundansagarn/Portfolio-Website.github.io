d5520099189e64723c889931daeadd8f522266eb
kundansagarr8 <sagarkundan444@gmail.com>
2024-03-25 14:32:12 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#text", {
+    strings:["Developer.", "Engineer.", "Designer.", "Lead.", "Consultant."],
+    backSpeed:60,
+    typeSpeed:100,
+    backDelay:1000,
+    loop:true
+});
